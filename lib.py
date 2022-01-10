9213a1dae6cd7e1c59ea830897d4a79aec3b1d97
Kaaize <71920355+Kaaize@users.noreply.github.com>
2022-01-10 19:33:25 -0300

    Initial commit

---
+++ lib.py
@@ -0,0 +1,271 @@
+from math import e
+from sqlite3.dbapi2 import Cursor
+from data import *
+from random import randint,choice
+import sqlite3
+#----------------FUNÇÕES
+def map(x, in_min, in_max, out_min, out_max):
+    return float((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
+
+def getExp(lv):
+    lv = lv-1
+    return ((50 * lv * lv * lv) - (150 * lv * lv) + (400 * lv)) / 3
+
+def getLvl(current: int, grande: int, media: int, pequena: int, tier: float):
+    level = None
+    exp_add = (grande*100000) + (media*10000) + (pequena*1000)
+    total_exp = current+(exp_add*tier)
+    for i in range(0,1001):
+        if total_exp >= getExp(i) and total_exp <= getExp(i+1):
+            level = i
+            exp_to_up = getExp(i+1) - getExp(i)
+            exceeded = total_exp - getExp(i)
+            percent_lvl = (1 - exceeded / exp_to_up) * 100
+            return (f"""```Exp Atingida: {total_exp}
+Level Atingido: {level}
+Exp P/ Upar: {exp_to_up-exceeded:0.0f}
+Porcentagem P/ Upar: {percent_lvl:0.0f}```""")
+    return (f"""Level maior que 1000.""")
+
+def getPots(current: int, target: int, tier: int):
+        pots = {'small':1000*tier,'med':10000*tier,'big':100000*tier}
+        true_needed = getExp(target)-current
+        needed = true_needed
+        big_needed = 0
+        med_needed = 0
+        small_needed = 0
+        while 1:
+            if needed > pots['big']:
+                big_needed += 1
+                needed -= pots['big']
+            elif needed > pots['med']:
+                med_needed += 1
+                needed -= pots['med']
+            elif needed > 0:
+                small_needed += 1
+                needed -= pots['small']
+            else:
+                break     
+        return(f"""```Exp Necessária: {true_needed:0.0f}
+Poções Grandes: {big_needed}
+Poções Médias: {med_needed}
+Poções Pequenas: {small_needed}```""")
+
+def getRecipe(nome, main_qty):
+    items = {
+            #cozinha
+            'geladeira': {
+                'tomate':[0,0], 'cogumelo':[0,0], 'ovo':[0,0], 'bacon':[0,0], 'folha verde':[0,0], 'leite':[0,0], 'agua':[0,0],
+				'queijo':[0,0], 'peixe cru':[0,0], 'bife cru':[0,0], 'limão':[0,0], 'vinho barato':[0,0], 'manteiga':[0,0],
+            },
+			 
+			'armario': {
+                'batata':[0,0], 'cebola':[0,0], 'azeite':[0,0], 'alho':[0,0], 'arroz':[0,0], 'farinha':[0,0], 'shoyu':[0,0], 'sal':[0,0],
+				'pimenta':[0,0], 'lata de atum':[0,0],
+            },
+			 
+			'tigela': {'tigela':[0,0]},
+			 
+			'rolo': {'massa':[0,0]},
+			 
+			'corte': {'pasta':[0,0]},
+			 
+			'panela': {
+                'ramen':[0,0], 'arroz cozido':[0,0], 'sopa de vegetais':[0,0], "sopa mista picante":[0,0], "ramen de carne":[0,0], "miso ramen":[0,0], "ramen misto":[0,0],
+				"ramen de galinha":[0,0], "ramen de camarão":[0,0], "lagosta prime":[0,0], "caranguejo cozido":[0,0]
+            },
+				
+			'tabua': {
+                "onigiri":[0,0], "sushi":[0,0], "nigiri":[0,0], "bento japones":[0,0], "dango":[0,0], "waffles":[0,0]
+            },
+			 
+			'forno':{
+                "bife prime":[0,0], "carne de porco gourmet":[0,0], "polvo gourmet com ovos":[0,0], "pizza":[0,0], "frango a parmegiana":[0,0], 
+				"frango a parmegiana com ovos":[0,0]
+            },
+                
+		    'frigideira':{
+                "bife frito":[0,0], "ovo frito":[0,0], "espeto de carne":[0,0], "kakuni japones prime":[0,0], "peixe grelhado gourmet":[0,0],
+				"espeto de peixe japones prime":[0,0], "takoyaki":[0,0], "gyoza":[0,0], "peixe apimentado picante":[0,0],
+                "coxa de frango frito":[0,0], "camarão empanado":[0,0]
+            },
+
+            'geladeira_aliança': {
+                "bife cru premium": [0,0], "lagosta crua": [0,0], "camarão cru": [0,0], "carne crua de coelho": [0,0], "ostra": [0,0], "alface": [0,0],
+                "vinho branco": [0,0], "trufa branca": [0,0], "creme de leite": [0,0], "atum":[0,0]
+            },
+
+            'menu_aliança': {
+                "bife e ovo de frigideira": [0,0], "medalhão de carne":  [0,0],
+                "salada italiana":  [0,0], "espeto de carne gourmet":  [0,0], "curry de coelho":  [0,0],
+                "camarões salteados": [0,0], "ensopado de ostra": [0,0],
+            },
+			 
+            #navio
+            
+            'npc': {
+                #Barco
+                    "tora de madeira":[0,0], "algodão de baixa qualidade":[0,0], "minério de cobre":[0,0], "pólvora":[0,0],
+                    "tora de carvalho": [0,0], "algodão": [0,0], "minério de ferro": [0,0], "pólvora melhorada": [0,0],
+                    "tora de mogno": [0,0], "algodão melhorado": [0,0], "minério de aço": [0,0], "pólvora superior": [0,0],
+                #Cozinha
+                    "carne de porco": [0,0], "perna de galinha": [0,0], "peru": [0,0], "tomate do deserto": [0,0],
+                    "folhas verdes especiais": [0,0], "açucar rosa": [0,0],  "carne de caranguejo": [0,0],
+            },
+
+            'refinaria': {
+                "prancha de madeira":[0,0], "lingote de cobre":[0,0], "pano de baixa qualidade":[0,0], "corda de baixa qualidade":[0,0], "prego de cobre":[0,0],
+                "prancha de carvalho":[0,0], "lingote de ferro":[0,0], "pano":[0,0], "corda":[0,0], "prego de ferro":[0,0],
+                "prancha de mogno":[0,0], "lingote de aço":[0,0], "pano melhorado":[0,0], "corda melhorada":[0,0], "prego de aço":[0,0],
+            },
+
+            'drop': {
+                "cola":[0,0], "cano de arma": [0,0], "bala de canhão": [0,0],
+                "bateria":[0,0], "tanque de ar": [0,0], "liquido inflamavel": [0,0], "cogumelo venenoso": [0,0], "lata de óleo": [0,0],
+                "grande cola": [0,0], "cano de arma superior": [0,0], "bala de canhão de aço": [0,0],
+            },
+
+            'mesa comum': {
+                #barco 1
+                    "canhão1":[0,0], "vela1":[0,0], "casco1":[0,0],
+                #barco 2
+                    "barco2":[0,0], "canhão2":[0,0], "vela2":[0,0], "casco2":[0,0], "espingarda2":[0,0], "tiro de canhão corrente2":[0,0],
+                     "ariete2":[0,0],
+                #barco 3
+                    "barco3":[0,0], "canhão3":[0,0], "vela3":[0,0], "casco3":[0,0], "espingarda3":[0,0], "tiro de canhão corrente3":[0,0], "ariete3":[0,0], 
+                    "metralhadora3":[0,0], "acelerar3":[0,0], "canhão pesado3":[0,0], 
+                #barco 4
+                    "barco4":[0,0], "canhão4":[0,0], "vela4":[0,0], "casco4":[0,0], 
+            },
+
+            'estaleiro': {
+                #Nivel 1
+                    "espingarda4":[0,0], "tiro de canhão corrente4":[0,0], "ariete4":[0,0], "metralhadora4":[0,0], "acelerar4":[0,0], 
+                    "canhão pesado4":[0,0],
+                #Nivel 2 Bronze
+                    "barril explosivo4":[0,0], "barril de óleo4":[0,0], "explosão de velocidade4":[0,0], "reforçar4":[0,0], 
+                #Nivel 2 Prata
+                    "bomba de veneno4":[0,0], "lança chamas4":[0,0], "bombardeiro4":[0,0], "bolha4":[0,0], "sonar4":[0,0], 
+                #Nivel 3     
+                    "barco5":[0,0], "canhão5":[0,0], "vela5":[0,0], "casco5":[0,0], 
+                    "espingarda5":[0,0], "tiro de canhão corrente5":[0,0], "ariete5":[0,0], "metralhadora5":[0,0], "acelerar5":[0,0], 
+                    "canhão pesado5":[0,0],
+            },
+
+			'outro': {
+                "tentaculo de bebe polvo":[0,0], "açucar especial":[0,0], "frango especial":[0,0], "açucar rosa":[0,0],
+                },             
+    }
+    for ing, qty in crafts[nome].items():
+        local = getLocal(ing)[0]
+        price = getLocal(ing)[1]
+        if ing in items[local]:
+            items[local][ing][0] += qty*main_qty
+            items[local][ing][1] += price*qty*main_qty
+        else:
+            items[local][ing][0] = qty*main_qty
+            items[local][ing][1] = price*qty*main_qty
+        if ing in crafts:
+            for place, recipe in getRecipe(ing, qty*main_qty).items():
+                for subing, subqty in recipe.items():
+                    if subing in items[place]:
+                        items[place][subing][0] += subqty[0]
+                        items[place][subing][1] += subqty[1]
+                    else:
+                        items[place][subing][0] = subqty[0]
+                        items[place][subing][1] = subqty[1]
+
+    return items
+
+def getLocal(nome):
+    for k, v in locais.items():
+        if nome.lower() in v:
+            return(k, v[nome])
+    return ('outro', '?')
+
+def reduction(arm, valor):
+    reduct = arm/(valor+arm)
+    return f"```Redução de dano: {reduct*100}%```"
+
+def effectiviness(life: int, red: float):
+    red = 1-(red/100)
+    efet = round(life/(life*red), 5)
+    efet_life = life*efet
+    return f"```Efetividade da Vida: {efet:.2f} \nVida Efetiva: {int(efet_life)}```"
+ 
+def InfoCharacter(name):
+    char = chars[name]
+    text = f"""```
+Nome: {char['name']}
+Tier: {char['tier']}
+Tags: {char['tags']}
+Vida Base: {char['life']}
+Vida/Vit: {char['vit']}
+Fator de Defesa: {char['def']}```"""
+    return text
+
+def BauRot(char1, char2, char3, keys):
+    keyn = 0
+    chance100 = (10,10,10,10,10,10,10,10,10,10,25,50,75,100)
+    chars = {char1: 0, char2: 0, char3: 0, 'others': 0}
+    for i in range(0,keys):
+        frags = 20
+        if randint(1,100) <= chance100[keyn]:
+            frags = 100
+            keyn = 13
+        chance = randint(1,100)
+        if chance <= 40:
+            choosen = choice([char1,char2,char3])
+            chars[choosen] += frags
+        else:
+            chars['others'] += frags
+        keyn += 1
+        if keyn >= 13:
+            keyn = 0
+    return chars
+
+
+def Boosts(start,end,try_cost, sky, wise, crimson):
+    chances = [35,30,25,20,22,18,14,10,10,9,8,7]
+    cap = [3,4,5,6,5,6,8,11,11,12,13,15]
+    cristais = [0,0,0]
+    cristais_name = ['Céu', 'Sábio', 'Carmesin']
+    tipo = try_cost
+    custo = 0
+    start_boost = start
+    final_boost = end
+    for i in range(0,10000):
+        tent = 0
+        boost = start_boost
+        while boost < final_boost:
+            for j in range(0,cap[boost]):
+                sorteio = randint(1,100)
+                #print(chances[boost], cap[boost], j+1, sorteio)
+                if sorteio <= chances[boost] or j+1 == cap[boost]:
+                    tent += 1
+                    if boost < 4:
+                        custo += tipo*sky
+                        cristais[0] += try_cost/10000
+                    elif boost < 8:
+                        custo += tipo*wise
+                        cristais[1] += try_cost/10000
+                    else:
+                        custo += tipo*crimson
+                        cristais[2] += try_cost/10000
+                    boost += 1
+                    break
+                else:
+                    tent += 1
+                    if boost < 4:
+                        custo += tipo*sky
+                        cristais[0] += try_cost/10000
+                    elif boost < 8:
+                        custo += tipo*wise
+                        cristais[1] += try_cost/10000
+                    else:
+                        custo += tipo*crimson
+                        cristais[2] += try_cost/10000
+    text = f'{int(custo//10000):0,}\n'.replace(',','.')
+    for i in range(0,3):
+        text += f'{cristais_name[i]}: {cristais[i]:.2f}\n'
+    return text
---
+++ main.py
@@ -0,0 +1,195 @@
+from discord.ext.commands.errors import BadArgument, BadBoolArgument
+from lib import *
+from discord.ext import commands
+import discord
+def main():
+    intents = discord.Intents.default()
+    intents.members = True
+   # bot = commands.Bot(command_prefix='?')
+    bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)
+    
+#----------------EVENTOS
+    @bot.event
+    async def on_ready():
+        print("BOT ONLINE!")
+    
+    @bot.event
+    async def on_member_join(member):
+        idchannel = 846191030164520970
+        msg = f"{member.mention} entrou no Servidor."
+        await bot.get_channel(idchannel).send(msg)
+
+    @bot.event
+    async def on_member_remove(member):
+        idchannel = 846191030164520970
+        msg = f"{member.name} deixou o Servidor."
+        await bot.get_channel(idchannel).send(msg)
+#----------------COMANDOS
+
+
+#----------------Começo do comando GetPots
+    @bot.command(brief="Calcula as Poções Necessárias", 
+                usage="<exp atual> <level desejado> <tier>", 
+                description="Calcula a quantidade de poções necessárias \
+                para atingir o level desejado. \nTIERS: \nDIAMANTE: 0.5 \nOURO: 1 \nPRATA: 2 \nBRONZE: 3")
+    async def getpots(ctx, current: int, target: int, tier: float):
+        if target < 1000:
+                await ctx.send(getPots(current,target,tier))
+        else:
+            await ctx.send("Level muito alto")
+        
+    @getpots.error
+    async def getpots_error(ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Argumentos invalidos, tente `!help getpots`")
+#----------------Fim do comando GetPots
+
+
+#----------------Começo do comando GetLvl
+    @bot.command(brief="Calcula o Level Atingido", 
+                usage="<exp atual> <poções grandes> <poções medias> <poções pequenas> <tier>", 
+                description="Calcula o level atingido ao usar a quantidade informada de poções. \
+                \nTIERS: \nDIAMANTE: 0.5 \nOURO: 1 \nPRATA: 2 \nBRONZE: 3")
+    async def getlvl(ctx, current: int, grande: int, media: int, pequena: int, tier: float):
+        await ctx.send(getLvl(current,grande, media, pequena, tier))
+
+    @getlvl.error
+    async def getlvl_error(ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Argumentos invalidos, tente `!help getlvl`")
+#----------------Fim do comando GetLvl
+
+#----------------Começo do comando Stars
+    @bot.command(brief="Tabela de estrelas (preço ou força)", 
+                usage="<tipo>", 
+                description="Mostra uma tabela de Estrelas (Preço ou Força).\nTipos:\npower\nprice")
+    async def stars(ctx):
+        await ctx.send(file=discord.File('imgs/stars_percent.png'))
+        await ctx.send(file=discord.File('imgs/stars.png'))
+        await ctx.send(file=discord.File('imgs/frags.png')) 
+#----------------Fim do comando Stars
+
+#----------------Começo do comando Craft
+    @bot.command(brief="Mostra a quantidade de itens para craftar", 
+                usage="<\"comida\"> <quantidade>", 
+                description="Mostra a quantidade de itens necessária para para Craftar algo na quantidade \
+                esolhida.\nEX: !craft \"ramen misto\" 10\n**Nomes com espaço precisam estar entre aspas.**")
+    async def craft(ctx, nome, qty = 1):
+        if qty > 1000:
+            await ctx.send("Quantidade muito alta")
+        else:
+            custo = 0
+            msg = ''
+            try:
+                x = getRecipe(nome.lower(), qty)
+                for local, recipes in x.items():
+                    for ing, qty in recipes.items():
+                        if isinstance(qty[0], int) and qty[0] > 0:
+                            msg += f"```>>>>>{local.upper()}<<<<<\n"
+                            break
+                    for ing, qty in recipes.items():
+                        if isinstance(qty[0], int) and qty[0] > 0:
+                            custo += qty[1]
+                            msg += f"{ing.capitalize()} - {qty[0]} > ${qty[1]}\n"
+                    for ing, qty in recipes.items():
+                        if isinstance(qty[0], int) and qty[0] > 0:
+                            msg += '```'
+                            break
+                await ctx.send(msg)
+                await ctx.send(f"Custo total: {custo}")
+            except:
+                await ctx.send("""Item não encontrado (utilize ç e ~).
+Para encontrar o nome correto do item, tente o comando `!buscarcraft <parte do nome>`
+EX (procurar o nome do item "Medalhão de carne"): `!buscarcraft medalh`
+""")
+    
+    @craft.error
+    async def craft_error(ctx, error):
+        if isinstance(error, BadArgument):
+            await ctx.send("Argumentos invalidos. \nPara nomes com espaço, use Aspas (\"sopa mista picante\") \
+            \ntente também `!help craft`")
+#----------------Fim do comando Craft
+
+#----------------Começo do comando Defense
+    @bot.command(brief="Calcula a redução de dano",
+                usage="<armor> <def factor>",
+                description="Calcula a redução de dano")
+    async def defense(ctx, armor: int, deff: int):
+        await ctx.send(reduction(armor,deff))
+
+    @defense.error
+    async def defense_error(ctx, error):
+        if isinstance(error, BadArgument):
+            await ctx.send("Argumento invalido, tente !help defense")
+#----------------Fim do comando Defense
+
+#----------------Começo do comando Effect
+    @bot.command(brief="Calcula a vida efetiva",
+                usage="<life> <dmg reduction>",
+                description="Calcula a vida efetiva")
+    async def effect(ctx, life: int, red: float):
+        await ctx.send(effectiviness(life, red))
+
+    @effect.error
+    async def effect_error(ctx, error):
+        if isinstance(error, BadArgument):
+            await ctx.send("Argumento invalido, tente !help effect")
+#----------------Fim do comando Effect
+
+#----------------Começo do Comando InfoChar
+    @bot.command(brief="Mostra informações sobre um personagem",
+                usage="<char>",
+                description="Mostra informações de um personagem.")
+    async def infochar(ctx, name):
+        await ctx.send(InfoCharacter(name))
+        
+    @infochar.error
+    async def infochar_error(ctx, error):
+        if isinstance(error, BadArgument):
+            await ctx.send("Argumento invalido.")
+#----------------Fim do Comando InfoChar
+
+#----------------Começo do Comando Bau
+    @bot.command(brief="Simula uma rotação de bau",
+                usage="<char1> <char2> <char3> <keyn>",
+                description="Simula um bau de rotação")
+    async def bau(ctx, char1, char2, char3, keyn: int):
+        c = BauRot(char1, char2, char3, keyn)
+        text = '```'
+        for k,v in c.items():
+            text += f"{k.capitalize()}: {v}\n"
+        text += '```'
+        await ctx.send(text)
+#----------------Fim do comando Bau
+
+#----------------Começo do Comando Boost
+    @bot.command(brief="Mostra a Média de preço para boostar um item de x até y",
+                usage="<boost_start> <boost_end> <try_cost> <sky_price> <wise_price> <crimson_price>",
+                description="Calcula o preço Médio para boostar um item de X até Y")
+    async def boost(ctx, start: int, end: int, try_cost: int, sky: float, wise: float, crimson: float):
+            text = Boosts(start, end, try_cost, sky, wise, crimson)
+            await ctx.send(f'```Custo Médio: {text}```')
+ 
+    @boost.error
+    async def boost_error(ctx, error):
+        if isinstance(error, BadArgument):
+            await ctx.send("Argumento invalido.")
+#----------------Fim do comando Boost
+
+    @bot.event
+    async def on_message(message):
+
+            if message.channel.name == 'bot_commands':
+                await bot.process_commands(message)
+            if message.channel.name == 'votação':
+                emotes = ['🟢']
+                for i in emotes:
+                    await message.add_reaction(i)
+
+
+#----------------INICIALIZAÇÃO DO BOT
+    bot.run("bot_key")
+
+
+#----------------EXECUÇÃO
+main()
